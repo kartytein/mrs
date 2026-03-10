cbb343e219f06c4526e140b9367bb5327ae8fb45
kartytein <lv.i.p.llion@gmail.com>
2026-03-10 16:01:05 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {
+Team = "Pirates",
+Configuration = {
+HopWhenIdle = true,
+AutoHop = true,
+AutoHopDelay = 60 * 60,
+FpsBoost = false,
+BlackScreen = true
+},
+Items = {
+AutoFullyMelees = false,
+Saber = true,
+CursedDualKatana = false,
+SoulGuitar = false,
+RaceV2 = true
+},
+Settings = {
+StayInSea2UntilHaveDarkFragments = false
+}
+}
