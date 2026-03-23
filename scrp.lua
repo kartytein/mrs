65ff56070641c970598f8a23da4f61022b96e23d
kartytein <lv.i.p.llion@gmail.com>
2026-03-24 00:19:29 +0300

    Update scrp.lua

--- scrp.lua
+++ scrp.lua
@@ -7,7 +7,7 @@ local level = data:WaitForChild("Level")
 -- Проверка уровня
 if level.Value < 50 then
     -- Запускаем основной скрипт (сервер хоп / кик)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/kartytein/mrs/refs/heads/main/scrp.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kartytein/mrs/refs/heads/main/scrp2.lua"))()
 else
     -- Запускаем скрипт отправки в Discord (детектор фруктов)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/kartytein/mrs/refs/heads/main/config.lua"))()
