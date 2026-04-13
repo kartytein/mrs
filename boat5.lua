8c2db782868819bb5a7ce351b0eb5e370440c85a
kartytein <lv.i.p.llion@gmail.com>
2026-04-13 17:49:55 +0300

    Create boat5.lua

---
+++ boat5.lua
@@ -0,0 +1,11 @@
+-- 1. Выбор команды Marines
+local function selectMarines()
+    local replicatedStorage = game:GetService("ReplicatedStorage")
+    local remotes = replicatedStorage:WaitForChild("Remotes")
+    local commF = remotes:WaitForChild("CommF_")
+    commF:InvokeServer("SetTeam", "Marines")
+    local modules = replicatedStorage:WaitForChild("Modules")
+    local eventService = modules:FindFirstChild("RE/OnEventServiceActivity")
+    if eventService then eventService:FireServer() end
+end
+selectMarines()
